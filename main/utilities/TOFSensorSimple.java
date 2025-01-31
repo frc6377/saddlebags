fde84a2db8c7cc9352eea5bf4b59ca619a78ef73
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2025-01-30 20:13:30 -0600

    Add LaserCan to TOFSensor Class

--- main/utilities/TOFSensorSimple.java
+++ main/utilities/TOFSensorSimple.java
@@ -6,11 +6,13 @@ package utilities;
 
 import static edu.wpi.first.units.Units.Millimeters;
 
+import au.grapplerobotics.LaserCan;
 import com.playingwithfusion.TimeOfFlight;
 import edu.wpi.first.hal.SimBoolean;
 import edu.wpi.first.hal.SimDevice;
 import edu.wpi.first.hal.SimDevice.Direction;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -19,15 +21,30 @@ import frc.robot.Robot;
 /** Add your docs here. */
 public class TOFSensorSimple {
   private static ShuffleboardTab sensorTab = Shuffleboard.getTab("sensors");
-  private TimeOfFlight sensor;
+  private TimeOfFlight TOFSensor;
+  private LaserCan LazerCan;
   private Distance threshold;
   private int id;
   private SimDevice sim;
   private SimBoolean simBeam;
 
-  public TOFSensorSimple(int ID, Distance threshold) {
+  public static enum TOFType {
+    PW_FUSION,
+    LASER_CAN
+  }
+
+  private TOFType TOF_Type;
+
+  public TOFSensorSimple(int ID, Distance threshold, TOFType TOF_Type) {
     this.id = ID;
-    sensor = new TimeOfFlight(this.id);
+    this.TOF_Type = TOF_Type;
+
+    if (TOF_Type == TOFType.LASER_CAN) {
+      LazerCan = new LaserCan(this.id);
+    } else {
+      TOFSensor = new TimeOfFlight(this.id);
+    }
+
     if (!Robot.isCompetition) {
       sensorTab.addString(
           "tof sensor " + this.id + " measurement", () -> getDistance().toShortString());
@@ -45,7 +62,11 @@ public class TOFSensorSimple {
   }
 
   public Distance getDistance() {
-    return Millimeters.of(this.sensor.getRange());
+    if (TOF_Type == TOFType.LASER_CAN) {
+      return Millimeters.of(LazerCan.getMeasurement().distance_mm);
+    } else {
+      return Millimeters.of(this.TOFSensor.getRange());
+    }
   }
 
   public boolean isBeamBroke() {
@@ -60,6 +81,11 @@ public class TOFSensorSimple {
   }
 
   public void blink() {
-    sensor.identifySensor();
+    if (TOF_Type == TOFType.PW_FUSION) {
+      TOFSensor.identifySensor();
+    } else {
+      DriverStation.reportError(
+          "You are trying to blink a non Playing With Fusion TOF Sensor!!", null);
+    }
   }
 }
